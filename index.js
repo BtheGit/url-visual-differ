27d65d67252425dc8c9989b0fcbabe29e92ea339
BtheGit <brendanwebdev@gmail.com>
2018-02-23 12:35:17 -0500

    Output folder and gitignore

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const puppeteer = require('puppeteer');
 const looksSame = require('looks-same');
+const fs = require('fs');
+
+// Set up folder structure
+const outputDirectory = './output'
+if (!fs.existsSync(outputDirectory)){
+    fs.mkdirSync(outputDirectory);
+}
+
+const defaultSettings = {
+    pathPartial: '/',
+    basisPath: 'https://www.cancer.gov',
+    altPath: 'localhost:3000',
+    outputPath: 'diff',
+    tolerance: 5,
+    width: 1025,
+    height: 1, // using fullpage means this is irrelevant but width without height seems to throw in setViewport
+    highlightColor: '#DD3300',
+    fullPage: true,
+    //mobile = false
+}
 
 const getScreenshot = async (url, page) => {
     await page.goto(url);
@@ -9,33 +29,42 @@ const getScreenshot = async (url, page) => {
     return screenshot;
 }
 
-
-const createDiff = async ({
-    pathPartial = '/',
-    basisPath = 'https://www.cancer.gov',
-    altPath = 'localhost:3000',
-    outputPath = 'diff',
-    tolerance = 5,
-    width = 1025,
-    height = 1,
-    highlightColor = '#DD3300'
+const diffTwoImages = async (page, {
+    basisPath,
+    altPath,
+    pathPartial,
+    outputPath,
+    highlightColor,
+    tolerance,
 }) => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    page.setViewport({
-        width,
-        height
-    })
     const img1 = await getScreenshot(`${basisPath}${pathPartial}`, page, 'img1.png')
     const img2 = await getScreenshot(`${altPath}${pathPartial}`, page, 'img2.png')
 
     await looksSame.createDiff({
         reference: img1,
         current: img2,
-        diff: `${outputPath}.png`,
+        diff: `output/${outputPath}.png`,
         highlightColor,
         tolerance
     }, (err) => {})    
+
+}
+
+const createDiff = async (customSettings) => {
+    const options = Object.assign({}, defaultSettings, customSettings);
+    const {
+        width,
+        height
+    } = options;
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setViewport({
+        width,
+        height
+    })
+
+    await diffTwoImages(page, options);
+
     await browser.close();
 }
 
@@ -44,5 +73,4 @@ createDiff({
     altPath: 'https://www-red-dev.cancer.gov',
     tolerance: 10,
     pathPartial: '/about-cancer/treatment/clinical-trials/search',
-    width: 1024
 });
