2d8ebadb42f3e3cb71de4e31b048a63a16b86b1c
BtheGit <brendanwebdev@gmail.com>
2018-02-24 11:29:05 -0500

    Prototype server added

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 8000;
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
