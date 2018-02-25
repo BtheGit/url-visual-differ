c05defb8de0b0136c015520c58ff31dc7b5f3f21
BtheGit <brendanwebdev@gmail.com>
2018-02-24 22:08:05 -0500

    Styling Form and Header

--- client/src/views/Diff/DiffForm.js
+++ client/src/views/Diff/DiffForm.js
@@ -1,21 +1,57 @@
 import React from 'react';
 import { reduxForm, Field } from 'redux-form';
+import './DiffForm.css';
 
 const DiffForm = ({ handleSubmit }) => (
-    <form onSubmit={ handleSubmit }>
-      <div>
-        <label htmlFor="pathPartial">Path Partial</label>
-        <Field name="pathPartial" component="input" type="text" />
+    <form className="diff-form" onSubmit={ handleSubmit }>
+
+      <div className="diff-form__group">
+        <label htmlFor="pathPartial" className="diff-form__label">Path Partial</label>
+        <div className="diff-form__field">
+          <Field 
+            name="pathPartial" 
+            component="input" 
+            type="text"
+            placeholder="/"
+          />
+        </div>
       </div>
-      <div>
-        <label htmlFor="basisRoot">Basis Root</label>
-        <Field name="basisRoot" component="input" type="text" />
+      <div className="diff-form__group">
+        <label htmlFor="basisRoot" className="diff-form__label">Basis Root</label>
+        <div className="diff-form__field">
+          <Field 
+            name="basisRoot" 
+            component="input" 
+            type="text" 
+            placeholder="https://www.cancer.gov"
+          />
+        </div>
       </div>
-      <div>
-        <label htmlFor="comparisonRoot">Comparison Root</label>
-        <Field name="comparisonRoot" component="input" type="text" />
+      <div className="diff-form__group">
+        <label htmlFor="comparisonRoot" className="diff-form__label">Comparison Root</label>
+        <div className="diff-form__field">
+          <Field 
+            name="comparisonRoot" 
+            component="input" 
+            type="text"
+            placeholder="localhost:3000"
+          />
+        </div>
       </div>
-      <button type="submit">Submit</button>
+      <div className="diff-form__group">
+        <label htmlFor="widths" className="diff-form__label">Array of target widths (comma separated)</label>
+        <div className="diff-form__field">
+          <Field 
+            name="widths" 
+            component="input" 
+            type="text" 
+            placeholder="320, 390, 391, 640, 641, 1024, 1025, 1441"
+          />
+        </div>
+      </div>
+      <div className="diff-form__submit">
+        <button type="submit">Submit</button>
+      </div>
     </form>
 )
 
