4aa55e2df11b9de4e045a226ed453ea63d6c3ab9
BtheGit <brendanwebdev@gmail.com>
2018-02-25 23:12:31 -0500

    Details page implementated pre-styling

--- client/src/views/Diff/components/ResultsCard.js
+++ client/src/views/Diff/components/ResultsCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './ResultsCard.css';
 
 // NOTE: Number(x) === Number(x) is a trick to check for a valid number that is not NaN
@@ -6,7 +7,9 @@ import './ResultsCard.css';
 
 const ResultsCard = ({ type, imgSrc }) => (
     <div className="results-card">
-        <h3 className="results-card__title">{ type }{ Number(type) === Number(type) ? 'px' : null }</h3>
+        <Link to={`/details/${type}`}>
+            <h3 className="results-card__title">{ type }{ Number(type) === Number(type) ? 'px' : null }</h3>
+        </Link>
         <div className="results-card__img">
             <img src={imgSrc} alt={`Diff results preview ${type}`}/>
         </div>
--- client/src/views/DiffDetail/index.js
+++ client/src/views/DiffDetail/index.js
@@ -1,6 +1,79 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import base64js from 'base64-js';
 import './index.css';
 
-const DiffDetail = () => <div>Detail Page</div>
+const generateImgSrc = bufferArray => {
+    const base64Pre = 'data:image/png;base64,';
+    const imgData = base64js.fromByteArray(bufferArray);
+    const imgSrc = `${base64Pre}${imgData}`;
+    return imgSrc;
+}
 
-export default DiffDetail;
+const DiffDetail = ({ match, results }) => {
+    const width = match.params.id;
+    let detailInfo;
+    if(results) {
+        detailInfo = results.find(el => el.width.toString() === width.toString());
+    }
+    const renderError = () => <div className="diff-detail__error">Details Not Found. Please try a new search</div>;
+    const renderSettings = ({
+        pathPartial,
+        tolerance
+    }, width) => {
+        return (
+            <div className="diff-detail__settings">
+                <h1>{pathPartial}</h1>
+                <h4>{width}{ Number(width) === Number(width) ? 'px' : null }</h4>
+                <h4>{tolerance}</h4>
+            </div>
+        )
+    }
+    
+    const renderDetails = ({
+            img1,
+            img2,
+            result,
+            settings
+        }) => {
+        const {
+            basisRoot,
+            comparisonRoot
+        } = settings;
+        return (
+            <div className="diff-detail__inner">
+                <div className="diff-detail__img">
+                    <div className="diff-detail__img-header">
+                        <h3>{basisRoot}</h3>
+                    </div>
+                    {img1 && <img src={generateImgSrc(img1.data)} alt="Base" />}
+                </div>
+                <div className="diff-detail__img">
+                    <div className="diff-detail__img-header">
+                        <h3>{comparisonRoot}</h3>
+                    </div>
+                    {img2 && <img src={generateImgSrc(img2.data)} alt="Comparison" />}
+                </div>
+                <div className="diff-detail__img">
+                    <div className="diff-detail__img-header">
+                        <h3>Diff</h3>
+                    </div>
+                    {result && <img src={generateImgSrc(result.data)} alt="Diff Result" />}
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div className="diff-detail">
+            { !detailInfo ? null : renderSettings(detailInfo.settings, detailInfo.width) }
+            { !detailInfo ? renderError() : renderDetails(detailInfo) }
+        </div>
+    )
+}
+
+const MapStateToProps = store => ({
+    results: store.diff.results
+})
+
+export default connect(MapStateToProps)(DiffDetail);
