62620a6b2079d7ae00415cee9f2a0ae70d099fc3
BtheGit <brendanwebdev@gmail.com>
2018-02-25 21:17:25 -0500

    React-router-redux initial implementation

--- client/src/index.js
+++ client/src/index.js
@@ -6,23 +6,31 @@ import thunk from 'redux-thunk';
 import diff from './store/diff/reducer'
 import { reducer as reduxFormReducer } from 'redux-form';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux'
+import { createBrowserHistory as createHistory } from 'history';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const history = createHistory();
+const historyMiddleware = routerMiddleware(history);
+
 const store = createStore(
     combineReducers({
         diff,
-        form: reduxFormReducer
+        form: reduxFormReducer,
+        router: routerReducer
     }),
     composeWithDevTools(
-        applyMiddleware(thunk)
+        applyMiddleware(thunk, historyMiddleware)
     )
 )
 
 const AppTree = () => (
-    <Provider store={store}>
-        <App />
+    <Provider store={ store }>
+        <ConnectedRouter history={ history }>
+            <App />
+        </ConnectedRouter>
     </Provider>
 )
 
